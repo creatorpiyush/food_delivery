f9f8809c37c8ead5750cdb0c90f044a808829800
Piyush Anand <piyushanand.1221@gmail.com>
2020-12-19 01:05:22 +0530

    initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-
 const port = process.env.PORT || 5555;
 
-app.listen(port, ()=>{
-    console.log(`Server started at http://localhost:${port}`);
-})
+app.use(express.urlencoded({ urlencoded: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
